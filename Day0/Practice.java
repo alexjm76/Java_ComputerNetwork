0a331c7b761242ed3f0822973ada4f076168ad48
Jungmoo Yoon <alexjm@naver.com>
2023-02-10 18:27:09 +0900

    조건문 1330번

--- Day0/Practice.java
+++ Day0/Practice.java
@@ -1,7 +1,28 @@
+
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hi my name is");
-        System.out.println("second line");
+        int A = 0;
+        int B = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.println("A값을 입력하세요: ");
+        A = sc.nextInt();
+        System.out.println("B값을 입력하세요: ");
+        B = sc.nextInt();
+        System.out.printf("%d %d\n",A,B);
+
+        if (A>B){
+            System.out.println(">");
+        } else if (A<B) {
+            System.out.println("<");
+        } else if (A==B) {
+            System.out.println("==");
+            
+        }
 
     }
 }
