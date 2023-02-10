a7eb08a3e460c55052fda41ea15c3fd7d60d098f
Jungmoo Yoon <alexjm@naver.com>
2023-02-10 18:49:18 +0900

    조건문 113751번

--- Day0/Practice.java
+++ Day0/Practice.java
@@ -3,24 +3,36 @@ import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-        int score = 0;
+        int x = 0;
+        int y = 0;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("시험 성적을 입력하세요 :");
-        score =sc.nextInt();
-        if (90<=score && score<=100){
-            System.out.println("A");
-        } 
-        else if (80<=score && score<=89) {
-            System.out.println("B");
-        } else if (70<=score && score<79) {
-            System.out.println("C");
-        } else if (60<=score && score<=69) {
-            System.out.println("D");
-            
+        System.out.print("x 좌표를 입력하세요: ");
+        x =sc.nextInt();
+
+        System.out.print("y 좌표를 입력하세요: ");
+        y =sc.nextInt();
+
+        System.out.printf("좌표 : (%d %d)\n",x,y);
+
+        if (x>0){
+            if(y>0){
+                System.out.println("1");
+            } else if (y<0) {
+                System.out.println("4");
+
+            }
+        } else if (x<0) {
+            if(y>0){
+                System.out.println("2");
+            } else if (y<0) {
+                System.out.println("3");
+
+            }
+
         }
         else {
-            System.out.println("F");
+            System.out.println("원점");
         }
 
 
