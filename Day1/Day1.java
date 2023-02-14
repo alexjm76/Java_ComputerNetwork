75299582eacd3dd0b89d0fe6c6f083191681bc29
Jungmoo Yoon <alexjm@naver.com>
2023-02-14 14:05:50 +0900

    EchoServer -v4

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -1,36 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Day1 {
     public static void main(String[] args) {
-        try {
-            URL url = new URL("www.google.com");
-            URLConnection urlConnection = url.openConnection();
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            br.close();
-        } catch (IOException ex) {
-            System.out.println("test");
-            // Handle exceptions
-        }
+        System.out.println("에코 서버");
+        try (ServerSocket serverSocket = new ServerSocket(6000)){
+            System.out.println("Waiting for connection.....");
+            Socket clientSocket = serverSocket.accept(); //접속대기
+            System.out.println("Connected to client");
+            try (
+                    BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true))
+            {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println("Server: " + line);
+                    pw.println(line); //클라이언트로 송신 out객체를 이용한 송신
+                }
 
-//        try {
-//            InetAddress addr = InetAddress.getByName("www.inha.ac.kr");
-//            //InetAddress addr = InetAddress.getByName("www.jungmoo.ac.kr"); 이렇게 하면 없는 주소이므로 에러뜸
-//            System.out.println(addr);
-//            System.out.println(addr.getCanonicalHostName()); //IP
-//            System.out.println(addr.getHostAddress()); //IP
-//            System.out.println(addr.getHostName()); //url
-//        } catch (UnknownHostException e) {
-//            throw new RuntimeException(e); //에러 만드는 함수
-//        }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);}
+        // Handle exceptions
     }
 }
--- Day1/SimpleEchoClient.java
+++ Day1/SimpleEchoClient.java
@@ -2,52 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class SimpleEchoClient {
     public static void main(String[] args) {
         System.out.println("에코 클라이언트 시작됨");
         try {
-            //InetAddress localAddress = InetAddress.getLocalHost(); //127.0.0.1
+            //InetAddress localAddr = InetAddress.getLocalHost();  // 127.0.0.1
             Socket clientSocket = null;
             PrintWriter pw = null;
-            BufferedReader br =null;
-
+            BufferedReader br = null;
             try{
-                //clientSocket = new Socket(localAddress, 6000); //로컬인 친구
-                clientSocket = new Socket("165.246.80.64", 6000);
+                clientSocket = new Socket("127.0.0.1", 20000);
                 pw = new PrintWriter(clientSocket.getOutputStream(), true);
                 br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                System.out.println("Connected to server");
-                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("서버에 연결됨");
+                Scanner sc = new Scanner(System.in);
                 while (true) {
-                    System.out.print("Enter text: ");
-                    String line = scanner.nextLine();
-                    if ("quit".equalsIgnoreCase(line)) { //IgnoreCase는 모든 문자 대소문자 신경쓰지 않는것
-                        break; //종료 조건
+                    System.out.print("전송 메세지 입력 : ");
+                    String line = sc.nextLine();
+                    if ("exit".equalsIgnoreCase(line)) {
+                        break;  // 종료 조건
                     }
-                    pw.println(line); //서버로 전송
-                    //서버로부터 수신받은 객체에서 라인단위로 문자열을 return
-
-                    System.out.println("Server response: " + br.readLine());
+                    pw.println(line);  // 서버로 전송
+                    // 서버로 부터 수신받은 객체에서 라인 단위로 문자열 리턴
+                    System.out.println("서버로 부터 받은 메세지 : " + br.readLine());
                 }
-
             } catch (IOException ex) {
-                System.out.println("입출력 예외발생");
-            } finally { //자원해제
-                clientSocket.close();
-                pw.close();
-                br.close();
+                System.out.println("입출력 예외 발생");
+            } finally {  // 자원 해제
+                if (clientSocket != null) clientSocket.close();
+                if (pw != null) pw.close();
+                if (br != null) br.close();
             }
-
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
